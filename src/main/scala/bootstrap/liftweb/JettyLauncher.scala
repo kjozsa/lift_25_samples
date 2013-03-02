0ac6364781c77e5394e6bb92773d4f74ce57958f
Kristof Jozsa <kristof.jozsa@gmail.com>
2013-03-02 18:45:45 +0100

    fix JettyLauncher

--- src/main/scala/bootstrap/liftweb/JettyLauncher.scala
+++ src/main/scala/bootstrap/liftweb/JettyLauncher.scala
@@ -1,16 +1,18 @@
+package bootstrap.liftweb
+
 import org.eclipse.jetty.server.Server
 
 import org.eclipse.jetty.servlet.{DefaultServlet, ServletContextHandler}
 import net.liftweb.http.LiftFilter
 import java.util.EnumSet
 import javax.servlet.DispatcherType
-import org.h2.server.web.WebServlet
 
 object JettyLauncher extends App {
   val port = Option(System.getenv("PORT")).getOrElse("8080").toInt
   val server = new Server(port)
-
   val context = new ServletContextHandler(server, "/", ServletContextHandler.SESSIONS)
+
+  context.addServlet(classOf[DefaultServlet], "/")
   context.addFilter(classOf[LiftFilter], "/*", EnumSet.of(DispatcherType.REQUEST))
   context.setResourceBase("src/main/webapp")
 
