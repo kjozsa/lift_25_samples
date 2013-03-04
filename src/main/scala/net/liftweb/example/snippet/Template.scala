2374b185dd2d85935649077f79896bbc90e23ee3
Kristof Jozsa <kristof.jozsa@gmail.com>
2013-03-04 16:51:13 +0100

    template sample

--- src/main/scala/net/liftweb/example/snippet/Template.scala
+++ src/main/scala/net/liftweb/example/snippet/Template.scala
@@ -19,7 +19,6 @@ package net.liftweb.example.snippet
 import _root_.net.liftweb._
 import http._
 import S._
-import common._
 import util._
 import Helpers._
 
@@ -28,32 +27,24 @@ import net.liftweb.example.model._
 import _root_.scala.xml.{NodeSeq, Text}
 
 object Template extends DispatchSnippet {
-  def dispatch: DispatchIt =
-  Map("show" -> show _)
+  def dispatch = {
+    case "show" => show _
+  }
 
-  def show(in: NodeSeq): NodeSeq = {
-    val ret: Box[NodeSeq] =
-    for (tmpl <- templateFromTemplateAttr;
-         (tbl, row) <- template(tmpl, "temp", "tbl", "row"))
-    yield {
-      val rows: NodeSeq =
-      User.findAll match {
-        case Nil => bind("item", row, "one" -> "No Records Found",
-                         "two" -> "")
-        case xs => xs.flatMap(u => bind("item", row,
-                                        "one" -> u.firstName.is,
-                                        "two" -> u.email.is))
+  def show(in: NodeSeq) = {
+    {
+      val users = User.findAll
+
+      users.length match {
+        case 0 => "#hasRecords" #> Text("No records found")
+        case _ =>
+          "#head-one *" #> "Name" &
+          "#head-two *" #> "Email" &
+          ".row *" #> (users.map { u =>
+            ".name *" #> u.firstName.is &
+            ".email *" #> u.email.is
+          })
       }
-
-      bind("head", tbl, "one" -> "Name",
-           "two" -> "Email",
-           "rows" -> rows)
-    }
-
-    ret match {
-      case Full(xs) => xs
-      case Empty => Text("Error processing template")
-      case Failure(msg, _, _) => Text("Error processing template: "+msg)
-    }
+    }.apply(templateFromTemplateAttr openOr Text("Error processing template"))
   }
 }
